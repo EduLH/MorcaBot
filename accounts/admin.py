5e1c356c838274a5d0b98a24f27192b49e0318b2
EduLH <eduardo_lh@protonmail.com>
2026-03-23 09:50:52 -0300

    Adicionando models de cupom e cupomredemption em admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Coupon, CouponRedemption
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Coupon)
+admin.site.register(CouponRedemption)
